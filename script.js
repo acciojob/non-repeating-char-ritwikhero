59436c11814c9f4471ee23c874ccedb4477d07a2
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-03-22 22:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Array(26).fill(0);
+
+	for(let i = 0; i < str.length; i++){
+		freq[str.charCodeAt(i) - 'a'.charCodeAt()]++;
+	}
+
+	for(let i = 0; i < str.length; i++){
+		if(freq[str.charCodeAt(i)-'a'.charCodeAt()] === 1){
+			return str[i];
+		}
+	}
+	return null
+	
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
